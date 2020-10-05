305ede8f47ebc88de1cda362fee8070ebc0d6cf3
Maryna21 <64320384+Maryna21@users.noreply.github.com>
2020-10-05 20:06:58 +0300

    task-7

--- js/task-7.js
+++ js/task-7.js
@@ -1,2 +1,6 @@
-const sizeControlEl = document.querySelector("input#font-size-control");
+const rangeEl = document.querySelector("#font-size-control");
 const spanEl = document.querySelector("#text");
+const onChangeRange = (event) => {
+  spanEl.style.fontSize = event.currentTarget.value + "px";
+};
+rangeEl.addEventListener("input", onChangeRange);
